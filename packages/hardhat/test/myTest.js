2d068c6b5cd1e8d17a562f223c7ab7f5f02b2a16
Joao Mateus Pinto Barbosa <joaomatbarbosa@gmail.com>
2021-08-14 00:09:31 +0100

    Update myTest.js

--- packages/hardhat/test/myTest.js
+++ packages/hardhat/test/myTest.js
@@ -22,7 +22,9 @@ describe("BBoard Contract", () => {
   describe('Create Token', () => {
     it('Should create a token and return his ID', async () => {
       const firstBlockID = await contract.CreateToken();
+      const secondBlockID = await contract.CreateToken();
       expect(firstBlockID).to.equal(1);
+      expect(secondBlockID).to.equal(2);
     });
 
     it('Should set Token Uri', async () => {
@@ -31,8 +33,9 @@ describe("BBoard Contract", () => {
       expect(tokenURI(firstBlockID)).to.equal(testUri);
     });
 
-    it('Should sell BBlock', async () => {
+    it('Should create BBlock sale', async () => {
       await contract.sellBBlock(firstBlockID, 5);
+      await contract.sellBBlock(secondBlockID, 5);
       expect(idToBBlock[firstBlockID].owner).to.equal(payable(address(this)));
       expect(idToBBlock[firstBlockID].seller).to.equal(payable(msg.sender));
       expect(idToBBlock[firstBlockID].price).to.equal(5);
@@ -43,6 +46,12 @@ describe("BBoard Contract", () => {
       expect(idToBBlock[firstBlockID].owner).to.equal(payable(msg.sender));
       expect(idToBBlock[firstBlockID].seller).to.equal(payable(address(0)));
       expect(idToBBlock[firstBlockID].price).to.equal(0);
+    });
+
+    it('Should buy a BBlock', async () => {
+      await contract.buyBBlock(secondBlockID);
+      expect(idToBBlock[secondBlockID].owner).to.equal(payable(msg.sender));
+      expect(idToBBlock[secondBlockID].seller).to.equal(payable(address(0)));
     });
 
   });
